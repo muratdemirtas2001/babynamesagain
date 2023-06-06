09edc2d0e9f7746a4eaa096d8f5431d4c2a19533
Murat Demirtas <muratdemirtas2001@gmail.com>
2023-06-06 20:06:00 +0100

    checking user token

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,13 +1,21 @@
 import LogoutButton from "./LogOutButton";
 import LoginButton from "./LoginBotton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Homepage = () => {
-    console.log("HELLO FROM HOMEPAGE");
+  console.log("HELLO FROM HOMEPAGE");
+  const { user } = useAuth0();
+  console.log(user);
+
   return (
     <>
+      {user && (
+        <>
+          <LoginButton />
+          <LogoutButton />{" "}
+        </>
+      )}
       <div>Welcometo the home page</div>
-      <LoginButton />
-      <LogoutButton />
     </>
   );
 };
